6e23a1859052d16f162e8e03a60e03351d176a1d
aesrubab <rubabhuseynova013@gmail.com>
2024-06-07 16:27:24 +0400

    classi colde yazdim

---
+++ ConsoleApp3/ATM.cs
@@ -0,0 +1,65 @@
+﻿namespace ConsoleApp3
+{
+    public class Atm
+    {
+        private decimal balance;
+        private string pin;
+        private int pinAttempts;
+
+        public Atm(decimal initialBalance, string initialPIN)
+        {
+            balance = initialBalance;
+            pin = initialPIN;
+            pinAttempts = 0;
+        }
+
+        public void EnteredPin(string enteredPIN)
+        {
+            if (pinAttempts >= 3)
+            {
+                throw new Exception("Hesabbb bloklandi!!");
+            }
+
+            if (enteredPIN == pin)
+            {
+                pinAttempts = 0;
+                Console.WriteLine("Hesaba daxil oldu");
+            }
+            else
+            {
+                pinAttempts++;
+                throw new Exception("Yalnis pin");
+            }
+        }
+
+        public void CheckBalance()
+        {
+            Console.WriteLine($"Balansınız:{balance} AZN");
+        }
+
+        public void cashOut(decimal amount)
+        {
+            if (amount > balance)
+            {
+                throw new Exception("Kifayet qeder balans yoxudr");
+            }
+            balance -= amount;
+            Console.WriteLine($"Yeni balans: {balance} AZN");
+        }
+
+        public void cashIn(decimal amount)
+        {
+            balance -= amount;
+            Console.WriteLine($"Yeni balans: {balance} AZN");
+        }
+        public void ChangePIN(string oldPIN, string newPIN)
+        {
+            if (oldPIN != pin)
+            {
+                throw new Exception("duzgun daxil edin");
+            }
+            pin = newPIN;
+            Console.WriteLine("pin deyisdirildi");
+        }
+    }
+}
--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,71 +1,7 @@
-﻿using System;
-using System.Numerics;
-using System.Xml.Serialization;
+﻿using ConsoleApp3;
 
 namespace AtmSystem
 {
-
-    public class Atm
-    {
-        private decimal balance;
-        private string pin;
-        private int pinAttempts;
-
-        public Atm(decimal initialBalance, string initialPIN)
-        {
-            balance = initialBalance;
-            pin = initialPIN;
-            pinAttempts = 0;
-        }
-
-        public void EnteredPin(string enteredPIN)
-        {
-            if (pinAttempts >= 3)
-            {
-                throw new Exception("Hesabbb bloklandi!!");
-            }
-
-            if (enteredPIN == pin)
-            {
-                pinAttempts = 0;
-                Console.WriteLine("Hesaba daxil oldu");
-            }
-            else
-            {
-                pinAttempts++;
-                throw new Exception("Yalnis pin");
-            }
-        }
-
-        public void CheckBalance()
-        {
-            Console.WriteLine($"Balansınız:{balance} AZN");
-        }
-
-        public void cashOut(decimal amount)
-        {
-            if (amount > balance)
-            {
-                throw new Exception("Kifayet qeder balans yoxudr");
-            }
-            balance -= amount;
-            Console.WriteLine($"Yeni balans: {balance} AZN");
-        }
-
-        public void cashIn(decimal amount) {
-            balance -= amount;
-            Console.WriteLine($"Yeni balans: {balance} AZN");
-        }
-        public void ChangePIN(string oldPIN, string newPIN)
-        {
-            if (oldPIN != pin)
-            {
-                throw new Exception("duzgun daxil edin");
-            }
-            pin = newPIN;
-            Console.WriteLine("pin deyisdirildi");
-        }
-    }
 
     class Program
     {
@@ -75,7 +11,6 @@ namespace AtmSystem
 
             Console.WriteLine("PIN kodunuzu daxil edin:");
             string pin = Console.ReadLine();
-            //adada
             try
             {
                 atm.EnteredPin(pin);
@@ -96,7 +31,7 @@ namespace AtmSystem
                 Console.WriteLine("5. Exit");
                 Console.Write("Seçim: ");
                 string choice = Console.ReadLine();
-
+                
                 try
                 {
                     switch (choice)
